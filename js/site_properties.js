2e048925e1473f25850376f86136665e031adb29
Rekha-15 <rekhapatil.1509@gmail.com>
2021-07-24 14:38:43 -0400

    added : converting employee payroll app to client server architecture : .

---
+++ js/site_properties.js
@@ -0,0 +1,5 @@
+let site_properties = {
+    use_local_storage: "false",
+    home_page: "../pages/homepage.html",  
+    add_emp_payroll_page: "../pages/EmpWage.html"     
+};
---
+++ js/utility.js
@@ -0,0 +1,25 @@
+const stringifyDate = (date) => {  
+const options = { day: 'numeric', month: 'short', year: 'numeric' };
+const newDate = !date ? "undefined" : new Date(Date.parse(date)).toLocaleDateString('en-GB', options);
+return newDate;
+}
+const checkName = (name) => { 
+let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+if (!nameRegex.test(name)) throw 'Name is Incorrect!';
+};
+
+const checkStartDate = (startDate) => { 
+let now = new Date();
+if (startDate > now) throw 'Start Date is a Future Date!';
+var diff = Math.abs(now.getTime() - startDate.getTime());
+    if (diff / (1000 * 60 * 60 * 24) > 30) 
+      throw 'Start Date is beyond 30 Days!';
+};
+
+toString=()=>{
+const options = { year: 'numeric', month: 'short', day: 'numeric' };
+const empDate = !this.startDate ? "undefined" : this.startDate;
+return ("id=" + this.id + ", name='" + this.name + ", gender='" + this.gender + 
+        ", profilePic='" + this.profilePic + ", department=" + this.department +
+        ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note);
+}
